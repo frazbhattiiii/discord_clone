9868092127f58de16857d617f52295de48dfe179
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2022-03-23 01:23:27 +0500

    Adding login

--- discord_frontend/src/authPages/LoginPage/LoginPage.js
+++ discord_frontend/src/authPages/LoginPage/LoginPage.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import AuthBox from '../../shared/components/AuthBox'
+import LoginPageFooter from './LoginPageFooter';
+import LoginPageHeader from './LoginPageHeader';
+import LoginPageInputs from './LoginPageInputs';
+import { validateLoginForm} from '../../shared/utils/validators';
+/**
+ * TODO
+ * 
+ */
 export default function LoginPage() {
+  const [mail,setMail]=useState("");
+  const [password,setPassword] = useState("");
+  const [isFormValid,setIsFormValid] = useState(false);
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({mail,password}));
+  },[mail,password,setIsFormValid])
+
+  const handleLogin=()=>{
+    // just for checking the input is displaying 
+    console.log(mail)
+    console.log(password)
+    console.log("log in") 
+  }
+
   return (
    <AuthBox>
-     Login Page
+     <LoginPageHeader/>
+     <LoginPageInputs mail={mail}
+     setMail={setMail}
+     password={password}
+     setPassword={setPassword}/>
+     <LoginPageFooter isFormValid={isFormValid} handleLogin={handleLogin}/>
    </AuthBox>
   )
 }
---
+++ discord_frontend/src/authPages/LoginPage/LoginPageHeader.js
@@ -0,0 +1,14 @@
+import {Typography} from '@mui/material';
+
+export default function LoginPageHeader() {
+  return (
+    <>
+    <Typography variant='h5' sx={{color:'white'}}>
+      Welcome Back!
+    </Typography>
+    <Typography sx={{color:'#b9bbbe'}}> 
+      We are happy you are with us
+    </Typography>
+    </>
+  )
+}

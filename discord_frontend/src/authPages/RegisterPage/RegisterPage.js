4c2ba13e868697e31e49850a8462d725281706a3
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2022-03-23 23:40:36 +0500

    adding register Page

--- discord_frontend/src/authPages/RegisterPage/RegisterPage.js
+++ discord_frontend/src/authPages/RegisterPage/RegisterPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import AuthBox from '../../shared/components/AuthBox'
+import AuthBox from '../../shared/components/AuthBox';
+import {Typography} from '@mui/material'
 export default function RegisterPage() {
   return (
    <AuthBox>
      {/* Everything in between this auth box will be the props.children there */}
-     Register Page
+     <Typography variant='5' sx={{
+       color:'white'
+     }}>
+       Create an Account
+     </Typography>
    </AuthBox>
   )
 }

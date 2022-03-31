9219a4780a256cbd3dc9bbaa964d6317c4b35a7c
Muhammad Ahmed Fraz <bhattifraz94@gmail.com>
2022-03-31 17:41:23 +0500

    Adding sidebar

--- discord_frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.js
+++ discord_frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.js
@@ -1,5 +1,27 @@
 import React from 'react'
 import {styled} from '@mui/system';
+import FriendListItem from './FriendListItem';
+
+const DummyFriends=[
+  {
+    id: 1,
+  name: 'John Doe',
+isOnline:true,
+},
+  {
+    id: 2,
+  name: 'Ahmed Fraz',
+isOnline:false,
+},
+  {
+    id: 3,
+  name: 'Bhatti Bhai',
+isOnline:false,
+},
+
+
+]
+
 const MainContainer = styled('div')({
 flexGrow:1,
 width:'100%'
@@ -8,7 +30,14 @@ width:'100%'
 function FriendsList() {
   return (
     <MainContainer>
-
+      {DummyFriends.map(friend=>(
+        <FriendListItem
+        name={friend.name}
+        id={friend.id}
+        key={friend.id}
+        isOnline={friend.isOnline}
+        />
+      ))}
 
     </MainContainer>
   )
